3b246aa100e54fe614b6047ffe334cf0bdefc39c
Natig Iskandarli <natigiskandarli@mac.home>
2025-02-23 20:44:13 -0500

    ss

--- src/app.ts
+++ src/app.ts
@@ -78,10 +78,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mount routes
 app.use(routes);
-
-console.log('Registered routes:', app._router.stack
+process.stdout.write('Registered routes: ' + JSON.stringify(app._router.stack
   .filter((r: any) => r.route)
-  .map((r: any) => `${r.route.path} - ${r.route.methods}`));
+  .map((r: any) => `${r.route.path} - ${r.route.methods}`)) + '\n');
 
 app.set('trust proxy', 1); // Proxy arxasında işləmək üçün
 
